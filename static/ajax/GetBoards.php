947383bcdb5511232ca267aebc8a0e00c00e70f4
Jokke <jonnejokke2@gmail.com>
2016-12-11 21:18:08 +0200

    Delete GetBoards.php

--- static/ajax/GetBoards.php
+++
@@ -1,8 +0,0 @@
-<?php require '../global.php';
-
-// Is the request coming from this website?
-$request->isCorrectReferer() or die;
-
-// These are public data also, so no need for hiding them?
-$board = BoardCore::getBoardController();
-echo json_encode($board->getBoards());
